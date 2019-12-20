7a17e6732fb46655092e77e32938ee142a110fa1
Forrest Bingham <bingham_5@live.com>
2019-12-19 17:04:40 -0700

    Added NOW

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 import UpdateMovie from "./Movies/UpdateMovie";
+import AddMovie from "./Movies/AddMovie";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -18,6 +19,7 @@ const App = () => {
       <SavedList list={savedList} />
       <Route exact path="/" component={MovieList} />
       <Route path='/update-movie/:id' render={props => {return <UpdateMovie {...props} />}} />
+      <Route path='/add-movie' render={props => { return <AddMovie {...props}/> }}/>
       <Route
         path="/movies/:id"
         render={props => {
